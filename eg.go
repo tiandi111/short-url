7754fb6e9706e467b06a1fab725091991c1a1c7f
Ubuntu <ubuntu@ip-172-26-5-133.ec2.internal>
2019-04-19 01:00:15 +0000

    server file

---
+++ eg.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type Generator struct {
+	index	int
+	pool 	map[int]string
+}
+
+func main() {
+	g := Generator{0, make(map[int]string, 0)}
+	fmt.Println(g.createURL("nihao"))
+	r := gin.Default()
+
+	r.GET("/:index", func(c *gin.Context) {
+		index, _:= strconv.Atoi( c.Param("index") )
+		c.Redirect(http.StatusMovedPermanently, g.pool[index])
+		fmt.Println("Redirect to: %s", g.pool[index])
+	})
+
+	r.POST("/:index/create", func(c *gin.Context) {
+		url := c.PostForm("url")
+		short := g.createURL(url)
+		c.String(http.StatusOK, short)
+		fmt.Println(short)
+	})
+
+	r.Run(":8080")
+}
+
+func (g *Generator) createURL(url string) string{
+	i := g.index
+	g.pool[i] = url
+	postfix := strconv.Itoa(i)
+	g.index++
+	return "http://54.196.113.135:8080/"+postfix
+}
